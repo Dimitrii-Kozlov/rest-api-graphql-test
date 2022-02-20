2c60876e010c088d0a6bcb3bc8b31ce45e73198e
Dimitrii-Kozlov <demetra_n@front.ru>
2022-02-20 17:34:57 +0300

    allow all hosts in settings.py

--- testapiproject/settings.py
+++ testapiproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-84=u6ou$%+u6*#3k$#1()22=8b(olb=a_he8la)widsu5eg*on
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
